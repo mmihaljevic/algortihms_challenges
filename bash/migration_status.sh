c1182c4b7a907bafbdb5dc86a456bfa84a610d39
Melita Mihaljevic <melita.mihaljevic@gmail.com>
2012-11-29 16:47:05 -0800

    fixed solution

--- bash/migration_status.sh
+++ bash/migration_status.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-while read a b
+
+while read host
 do
-nc -v -z -w 1 $a $b
-done<hosts.txt
+
+if nc -w 1 -z $host $2 &> /dev/null; then 
+    echo $host $2 
+fi
+
+if nc -w 1 -z $host $3 &> /dev/null; then           
+    echo $host $3 
+fi
+done<$1
